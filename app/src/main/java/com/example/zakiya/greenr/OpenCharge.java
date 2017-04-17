a1b0984abdbebb15d87950e4675e0e995015abc4
Arize Lee <arizelee@hotmail.com>
2017-04-16 22:27:15 -0400

    Merge branch 'master' of https://github.com/paceuniversity/CS3892017teamZACS
    Retrieve Location for OpenCharge Map API.
    
    # Conflicts:
    #	.idea/misc.xml
    #	app/build.gradle
    #	app/src/main/res/layout/activity_find_station.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/activity_splash.xml

--- app/src/main/java/com/example/zakiya/greenr/OpenCharge.java
+++ app/src/main/java/com/example/zakiya/greenr/OpenCharge.java
@@ -99,7 +99,7 @@ public class OpenCharge extends AppCompatActivity implements GoogleApiClient.Con
 
     private void requestLocationUpdates() {
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
+            // TO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
