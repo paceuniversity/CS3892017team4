9fa1a09323ea8c08314a2ba86323f02335809f4d
Arize Lee <arizelee@hotmail.com>
2017-04-18 18:15:52 -0400

    Merge branch 'master' of https://github.com/paceuniversity/CS3892017teamZACS
    # Conflicts:
    #	app/src/main/java/com/example/zakiya/greenr/mapScreen.java

--- app/src/main/java/com/example/zakiya/greenr/mapScreen.java
+++ app/src/main/java/com/example/zakiya/greenr/mapScreen.java
@@ -19,9 +19,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.zakiya.greenr.content.ChargingStation;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
@@ -39,6 +46,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -46,35 +55,48 @@ import java.util.Locale;
 
 public class mapScreen extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
-   private GoogleApiClient mGoogleApiClient;
-   private FusedLocationProviderApi locationProviderApi = LocationServices.FusedLocationApi;
-   private LocationRequest locationRequest;
-   protected static final String TAG = "MainActivity";
-   GoogleMap mGoogleMap;
-   MapFragment mapFragment = null;
-   private Double myLat;
-   private Double myLong;
-
-    TextView mLatitudeText;
-    TextView mLongitudeText;
-    String mLatitudeLabel, mLongitudeLabel;
+    private GoogleApiClient mGoogleApiClient;
+    private FusedLocationProviderApi locationProviderApi = LocationServices.FusedLocationApi;
+    private LocationRequest locationRequest;
+    protected static final String TAG = "MainActivity";
+    GoogleMap mGoogleMap;
+    MapFragment mapFragment = null;
+    private Double myLat;
+    private Double myLong;
+    private static final String url = "https://api.openchargemap.io/v2/poi/?output=json&countrycode=US&latitude=40.8357333&longitude=-73.9141208&distance=30&maxresults=1&compact=true&verbose=false&camelcase=true";
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (googleServicesAvailable()) {
             setContentView(R.layout.map_screen);
+            requestQueue = Volley.newRequestQueue(this);
+
             buildGoogleApiClient();
 
             locationRequest = new LocationRequest();
             locationRequest.setInterval(30000);
-            locationRequest.setFastestInterval(8000);
+            locationRequest.setFastestInterval(30000);
             locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
             initMap();
-            //requestLocationUpdates();
+
+            JsonArrayRequest arrayRequest = new JsonArrayRequest(url,
+                    new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG).show();
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Volley", "Error");
+                }});
+            requestQueue.add(arrayRequest);
+            }
         }
-    }
+        //makeJSONArrayRequest();
 
     protected synchronized void buildGoogleApiClient() {
         if (mGoogleApiClient == null) {
@@ -109,7 +131,7 @@ public class mapScreen extends AppCompatActivity implements OnMapReadyCallback, 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
-        goToLocationZoom(40.7131212,-74.0006327,15);
+        goToLocationZoom(40.7131212, -74.0006327, 15);
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -132,13 +154,14 @@ public class mapScreen extends AppCompatActivity implements OnMapReadyCallback, 
         for (int i = 0; i < favoritesList.size(); i++) {
             String location = favoritesList.get(i).getLocation();
 
-            try{
+            try {
                 results = geocoder.getFromLocationName(location, 1);
-            }catch(IOException ioException){}
+            } catch (IOException ioException) {
+            }
 
             double stationLat = results.get(0).getLatitude();
             double stationLong = results.get(0).getLongitude();
-            googleMap.addMarker(new MarkerOptions().position(new LatLng(stationLat,stationLong))
+            googleMap.addMarker(new MarkerOptions().position(new LatLng(stationLat, stationLong))
                     .title(favoritesList.get(i).getStationName())
                     .icon(BitmapDescriptorFactory.defaultMarker(130)));
         }
@@ -163,13 +186,13 @@ public class mapScreen extends AppCompatActivity implements OnMapReadyCallback, 
         mGoogleMap.moveCamera(update);
     }
 
-    public void geoLocate (View view) throws IOException{
+    public void geoLocate(View view) throws IOException {
         EditText et = (EditText) findViewById(R.id.editText);
         String location = et.getText().toString();
 
         Geocoder gc = new Geocoder(this);
-        List<Address> list = gc.getFromLocationName(location,1);
-        android.location.Address address =list.get(0);
+        List<Address> list = gc.getFromLocationName(location, 1);
+        android.location.Address address = list.get(0);
         String locality = address.getLocality();
 
         Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
@@ -181,7 +204,9 @@ public class mapScreen extends AppCompatActivity implements OnMapReadyCallback, 
     }
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {requestLocationUpdates();}
+    public void onConnected(@Nullable Bundle bundle) {
+        requestLocationUpdates();
+    }
 
     @Override
     public void onConnectionSuspended(int i) {
@@ -200,11 +225,11 @@ public class mapScreen extends AppCompatActivity implements OnMapReadyCallback, 
     public void onLocationChanged(Location location) {
         myLat = location.getLatitude();
         myLong = location.getLongitude();
-        Toast.makeText(this, myLat + " " + myLong, Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, myLat + " " + myLong, Toast.LENGTH_LONG).show();
 
         mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(myLat, myLong)).title("Your Location"));
 
-        if(myLat == null || myLong== null) {
+        if (myLat == null || myLong == null) {
             Toast.makeText(this, "no location detected", Toast.LENGTH_LONG).show();
         }
     }
@@ -253,13 +278,32 @@ public class mapScreen extends AppCompatActivity implements OnMapReadyCallback, 
     }
 
     //Launches the google maps app with the given coordinates and starts navigation
-    public void navigate(double sourceLatitude, double sourceLongitude, double destLatitude, double destLongitude){
+    public void navigate(double sourceLatitude, double sourceLongitude, double destLatitude, double destLongitude) {
         String sLat = Double.toString(sourceLatitude);
         String sLng = Double.toString(sourceLongitude);
         String dLat = Double.toString(destLatitude);
         String dLng = Double.toString(destLongitude);
         Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
-                Uri.parse("http://maps.google.com/maps?saddr="+sLat+","+sLng+"&daddr="+dLat+","+dLng));
+                Uri.parse("http://maps.google.com/maps?saddr=" + sLat + "," + sLng + "&daddr=" + dLat + "," + dLng));
         startActivity(intent);
     }
+
+
+/*    private void makeJSONArrayRequest() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d(TAG, response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("HTTP ERROR.");
+                error.printStackTrace();
+            }
+        });
+
+        RequestApp.getInstance().addToRequestQueue(jsonArrayRequest);
+    }*/
+
 }
