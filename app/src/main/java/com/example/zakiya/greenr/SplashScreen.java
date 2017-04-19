446d82401b35343294d48d3f4f1702c7cb4ebd30
zak1019010 <zak1019010@yahoo.com>
2017-04-19 11:35:35 -0400

    added image to splash page background

--- app/src/main/java/com/example/zakiya/greenr/SplashScreen.java
+++ app/src/main/java/com/example/zakiya/greenr/SplashScreen.java
@@ -6,24 +6,32 @@ package com.example.zakiya.greenr;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.VideoView;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.os.FileUriExposedException;
+import android.net.Uri;
 
 public class SplashScreen extends Activity {
 
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 3000;
+
+    private static int SPLASH_TIME_OUT = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        final VideoView videoView = (VideoView) findViewById(R.id.VideoView);
-        videoView.setVideoPath(
-                "User/user/AndroidStudioProjects/Greenr/app/src/main/res/drawable/splashVideo.mp4");
-
+        /*
+        final VideoView videoView = (VideoView) findViewById(R.id.splashVid);
+        Uri uri = Uri.parse("android.resource://"+ getPackageName() + "/" + R.raw.splashvideo);
+        videoView.setVideoURI(uri);
         videoView.start();
+        */
         new Handler().postDelayed(new Runnable() {
 
 
@@ -32,8 +40,8 @@ public class SplashScreen extends Activity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                startActivity(new Intent(SplashScreen.this, MainActivity.class));
-
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
                 // close this activity
                 SplashScreen.this.finish();
             }
